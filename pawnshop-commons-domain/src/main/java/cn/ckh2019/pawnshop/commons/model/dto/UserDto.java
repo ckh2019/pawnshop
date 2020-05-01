7d1dd70009dea00ec549eb1876ce732f9149deae
ckh2019 <chenkh2015@126.com>
2020-05-01 17:18:48 +0800

    delete

--- pawnshop-commons-domain/src/main/java/cn/ckh2019/pawnshop/commons/model/dto/UserDto.java
+++
@@ -1,8 +0,0 @@
-package cn.ckh2019.pawnshop.commons.model.dto;
-
-/**
- * @author Chen Kaihong
- * 2020-02-13 22:23
- */
-public class UserDto {
-}
--- pawnshop-commons-domain/src/main/java/cn/ckh2019/pawnshop/commons/model/pojo/User.java
+++
@@ -1,8 +0,0 @@
-package cn.ckh2019.pawnshop.commons.model.pojo;
-
-/**
- * @author Chen Kaihong
- * 2020-02-13 22:23
- */
-public class User {
-}
